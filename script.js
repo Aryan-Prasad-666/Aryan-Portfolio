22342f91b612dafd62749e02bebc30f2d07c52aa
Aryan-Prasad-666 <aryanprasad6aug@gmail.com>
2024-02-03 14:02:32 +0530

    added testimonials section

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function closeSidebar() {
     sidebar.classList.remove('show');
 }
 
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
